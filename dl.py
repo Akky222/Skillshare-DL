f48f0216d90cee349e0ef0cad5193c066151af02
Mark Jesper Pillerva <83439062+mark123jesper@users.noreply.github.com>
2022-03-02 19:16:45 +0800

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,12 +1,10 @@
 import sys, os
 from skillshare import Skillshare, splash
-from magic import cookie
-
 # or by class ID:
 # dl.download_course_by_class_id(189505397)
 
 def main():
-    dl = Skillshare(cookie)
+    dl = Skillshare("PHPSESSID=d0cfefa363fe3cef2460f8755d089a8e")
     course_url = sys.argv[1]
     dl.download_course_by_url(course_url)
 
